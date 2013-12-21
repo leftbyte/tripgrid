69439a54d51b5f9bfd1064d7e63d2e3461f9e495
Dan Phung <leftbyte@gmail.com>
2013-12-21 14:19:10 -0700

    Added time slices, need to refactor the edge checks.

--- TripQuery.py
+++ TripQuery.py
@@ -18,8 +18,18 @@ def log(loglevel, *args):
     if loglevel <= g_debugLevel:
         print args
 
-def SearchLeftEdge(x, y0, y1, type):
-    additionalTrips = []
+# XXX TODOs
+# XXX One overlapping inefficiency on the corners of the search.  They're always
+# checked twice.
+# XXX The only difference between the 4 edge search functions are the indices
+# (which can be generated further, and the comparison.  We should be able to
+# refactor, but it's nice having the debug logging right now...
+#
+# XXX: How would I define a "struct" to pass around instead of
+# all these params (bl, tr, type, t0, t1, withFare?
+#
+# XXX: should try passing in 'operator'
+def SearchLeftEdge(x, y0, y1, type, t0, t1, withFare, results):
     gridX = latitudeToGridQueue(x)
     gridY0 = longitudeToGridQueue(y0)
     gridY1 = longitudeToGridQueue(y1)
@@ -27,23 +37,26 @@ def SearchLeftEdge(x, y0, y1, type):
     log (5, "pruning left edge with x = %d from %d:%d" % (x, y0, y1))
     for gridY in yRange:
         key = getTripGridKey(gridX, gridY, type)
-        tripIDs = r.zrange(key, 0, -1)
+        tripIDs = r.zrangebyscore(key, t0, t1)
         log(5, "  trips in key %r: %r" % (key, tripIDs))
         for id in tripIDs:
-            locations = r.zrange(getTripKey(int(id), "locations"), 0, -1)
-            log(5, "  finding locations in trip %r where %d <= %r" % (id, x, locations))
+            locations = r.zrangebyscore(getTripKey(int(id), "locations"), t0, t1)
+            log(5, "  finding locations in trip %r where x: %d <= %r" % (id, x, locations))
+
             for l in locations:
                 locStr = l.strip("()").split(",")
-                log(7, "  comparing %d <= %d" % (x, int(locStr[0])))
-                if (x <= int(locStr[0])):
-                    log(5, "  found location in left edge %d %d %d <= %r" % (x, y0, y1, l))
-                    additionalTrips.append(id)
+                locInt = [int(_x) for _x in locStr]
+                log(7, "  comparing %d <= %d" % (locInt[0], x))
+                if (x <= locInt[0]):
+                    log(5, "  found location in left edge %d <= %d and %d <= %d <= %d" %
+                        (x, locInt[0], y0, locInt[1], y1))
+                    results['trips'].append(id)
+                    if withFare:
+                        farekey = getTripKey(int(id), "fare")
+                        results['fare'] += int(r.get(farekey))
                     break
-    log (5, "additional left edge trips:", additionalTrips)
-    return additionalTrips
 
-def SearchRightEdge(x, y0, y1, type):
-    additionalTrips = []
+def SearchRightEdge(x, y0, y1, type, t0, t1, withFare, results):
     gridX = latitudeToGridQueue(x)
     gridY0 = longitudeToGridQueue(y0)
     gridY1 = longitudeToGridQueue(y1)
@@ -51,24 +64,25 @@ def SearchRightEdge(x, y0, y1, type):
     log (5, "pruning right edge with x = %d from %d:%d" % (x, y0, y1))
     for gridY in yRange:
         key = getTripGridKey(gridX, gridY, type)
-        tripIDs = r.zrange(key, 0, -1)
+        tripIDs = r.zrangebyscore(key, t0, t1)
         log(5, "  trips in key %r: %r" % (key, tripIDs))
         for id in tripIDs:
-            locations = r.zrange(getTripKey(int(id), "locations"), 0, -1)
-            log(5, "  finding locations in trip %r where %d >= %r" % (id, x, locations))
+            locations = r.zrangebyscore(getTripKey(int(id), "locations"), t0, t1)
+            log(5, "  finding locations in trip %r where x: %d >= %r" % (id, x, locations))
             for l in locations:
                 locStr = l.strip("()").split(",")
-                log(7, "  comparing %d <= %d" % (x, int(locStr[0])))
-                if (x >= int(locStr[0])):
-                    log(5, "  found location in right edge %d %d %d >= %r" % (x, y0, y1, l))
-                    additionalTrips.append(id)
+                locInt = [int(_x) for _x in locStr]
+                log(7, "  comparing %d <= %d" % (locInt[0], x))
+                if (locInt[0] <= x):
+                    log(5, "  found location in right edge %d <= %d and %d <= %d <= %d" %
+                        (locInt[0], x, y0, locInt[1], y1))
+                    results['trips'].append(id)
+                    if withFare:
+                        farekey = getTripKey(int(id), "fare")
+                        results['fare'] += int(r.get(farekey))
                     break
 
-    log (5, "additional right edge trips:", additionalTrips)
-    return additionalTrips
-
-def SearchBottomEdge(y, x0, x1, type):
-    additionalTrips = []
+def SearchBottomEdge(y, x0, x1, type, t0, t1, withFare, results):
     gridY = longitudeToGridQueue(y)
     gridX0 = latitudeToGridQueue(x0)
     gridX1 = latitudeToGridQueue(x1)
@@ -76,23 +90,25 @@ def SearchBottomEdge(y, x0, x1, type):
     log (5, "pruning bottom edge with y = %d from %d:%d" % (y, x0, x1))
     for gridX in xRange:
         key = getTripGridKey(gridX, gridY, type)
-        tripIDs = r.zrange(key, 0, -1)
+        tripIDs = r.zrangebyscore(key, t0, t1)
         log(5, "  trips in key %r: %r" % (key, tripIDs))
         for id in tripIDs:
-            locations = r.zrange(getTripKey(int(id), "locations"), 0, -1)
-            log(5, "  finding locations in trip %r where %d <= %r" % (id, y, locations))
+            locations = r.zrangebyscore(getTripKey(int(id), "locations"), t0, t1)
+            log(5, "  finding locations in trip %r where: y: %d <= %r" % (id, y, locations))
             for l in locations:
                 locStr = l.strip("()").split(",")
-                log(7, "  comparing %d <= %d" % (y, int(locStr[1])))
-                if (y <= int(locStr[1])):
-                    log(5, "  Found location above bottom edge %d %d %d <= %r" % (y, x0, x1, l))
-                    additionalTrips.append(id)
+                locInt = [int(_y) for _y in locStr]
+                log(7, "  comparing %d <= %d" % (y, locInt[1]))
+                if (y <= locInt[1]):
+                    log(5, "  found location above bottom edge %d <= %d and %d <= %d <= %d" %
+                        (y, locInt[1], x0, locInt[0], x1))
+                    results['trips'].append(id)
+                    if withFare:
+                        farekey = getTripKey(int(id), "fare")
+                        results['fare'] += int(r.get(farekey))
                     break
-    log (5, "additional bottom edge trips:", additionalTrips)
-    return additionalTrips
 
-def SearchTopEdge(y, x0, x1, type):
-    additionalTrips = []
+def SearchTopEdge(y, x0, x1, type, t0, t1, withFare, results):
     gridY = longitudeToGridQueue(y)
     gridX0 = latitudeToGridQueue(x0)
     gridX1 = latitudeToGridQueue(x1)
@@ -100,54 +116,38 @@ def SearchTopEdge(y, x0, x1, type):
     log (5, "pruning top edge with y = %d from %d:%d" % (y, x0, x1))
     for gridX in xRange:
         key = getTripGridKey(gridX, gridY, type)
-        tripIDs = r.zrange(key, 0, -1)
+        tripIDs = r.zrangebyscore(key, t0, t1)
         log(5, "  trips in key %r: %r" % (key, tripIDs))
         for id in tripIDs:
-            locations = r.zrange(getTripKey(int(id), "locations"), 0, -1)
-            log(5, "  finding locations in trip %r where %d >= %r" % (id, y, locations))
+            locations = r.zrangebyscore(getTripKey(int(id), "locations"), t0, t1)
+            log(5, "  finding locations in trip %r where y: %d >= %r" % (id, y, locations))
             for l in locations:
                 locStr = l.strip("()").split(",")
-                log(7, "  comparing %d <= %d" % (y, int(locStr[1])))
-                if (y >= int(locStr[1])):
-                    log(5, "  Found location below top edge %d %d %d <= %r" % (y, x0, x1, l))
-                    additionalTrips.append(id)
+                locInt = [int(_y) for _y in locStr]
+                log(7, "  comparing %d <= %d" % (locInt[1], y))
+                if (locInt[1] <= y):
+                    log(5, "  found location below top edge %d <= %d and %d <= %d <= %d" %
+                        (locInt[1], y, x0, locInt[0], x1))
+                    results['trips'].append(id)
+                    if withFare:
+                        farekey = getTripKey(int(id), "fare")
+                        results['fare'] += int(r.get(farekey))
                     break
-    log (5, "additional bottom edge trips:", additionalTrips)
-    return additionalTrips
 
-def CheckEdges(bl, tr, type):
+def CheckEdges(bl, tr, type, t0, t1, withFare, results):
     '''
     Check the edges of our bounding box and inspect each tripID queue
     to make sure the trip should be included in our trip count.
     '''
-    # If the edges of the bounding box land on our queue edges, then we don't
-    # need to inspect each trip queue since the locations should be accurate.
-    delta = (g_maxLatitude - g_minLatitude) / g_numLatQueues
-    lat_left_edges = range(g_minLatitude, g_maxLatitude, delta)
-    lat_right_edges = [x + delta for x in range(g_minLatitude, g_maxLatitude, delta)]
-    long_bottom_edges = range(g_minLongitude, g_maxLongitude, delta)
-    long_top_edges = [x + delta for x in range(g_minLongitude, g_maxLongitude, delta)]
+    SearchLeftEdge(bl[0], bl[1], tr[1], type, t0, t1, withFare, results)
+    SearchRightEdge(tr[0], bl[1], tr[1], type, t0, t1, withFare, results)
+    SearchBottomEdge(bl[1], bl[0], tr[0], type, t0, t1, withFare, results)
+    SearchTopEdge(tr[1], bl[0], tr[0], type, t0, t1, withFare, results)
 
-    additionalTrips = []
-    # log(2, "XXX", bl[0], "in", lat_left_edges)
-    # if bl[0] not in lat_left_edges:
-    additionalTrips += SearchLeftEdge(bl[0], bl[1], tr[1], type)
-    # log(2, "XXX", tr[0], "in", lat_right_edges)
-    # if tr[0] not in lat_right_edges:
-    additionalTrips += SearchRightEdge(tr[0], bl[1], tr[1], type)
-    # log(2, "XXX", bl[1], "in", long_bottom_edges)
-    # if bl[1] not in long_bottom_edges:
-    additionalTrips += SearchBottomEdge(bl[1], bl[0], tr[0], type)
-    # log(2, "XXX", tr[1], "in", long_top_edges)
-    # if tr[1] not in long_top_edges:
-    additionalTrips += SearchTopEdge(tr[1], bl[0], tr[0], type)
-    log(5, "additional trips added", additionalTrips)
-    return additionalTrips
-
-def TripsInRect(bl, tr):
+def QueryRect(bl, tr, type, t0, t1, withFare, results):
     '''
-    Query how many trips passed through a given geo-rect, as defined by the
-    (bottom,left) and (top,right) points.
+    Query the trip database for the number of trips taken and the fare given a
+    rect and time slice.
     '''
     # First we generate the list of grid locations we want to query
     blX = latitudeToGridQueue(bl[0])
@@ -155,43 +155,66 @@ def TripsInRect(bl, tr):
     trX = latitudeToGridQueue(tr[0])
     trY = longitudeToGridQueue(tr[1])
 
-    # XXX: we should check the indicies and makes sure we're still legit after
-    # the subtraction of the edges.
-
     # We have to check the edges separately.
+    # XXX this is an edge case we need to handle
+    if blX == trX or blY == trY:
+        raise Exception("Error: Bad grid queue indices.")
     xRange = range(blX + 1, trX)
     yRange = range(blY + 1, trY)
-    trips = []
     for x in xRange:
         for y in xRange:
-            key = getTripGridKey(x, y, "ALL")
-            log(5, "  XXX key %r" % (key,))
-            newtrips = r.zrange(key, 0, -1)
+            key = getTripGridKey(x, y, type)
+            newtrips = r.zrangebyscore(key, t0, t1)
             log(5, "  trips added for %r: %r" % (key, newtrips))
-            trips += newtrips
+            if len(newtrips) == 0:
+                continue
+            results['trips'] += newtrips
+            if withFare:
+                farekey = getTripGridKey(x, y, "END:fare")
+                fare = r.get(farekey)
+                if fare is not None:
+                    results['fare'] += int(fare)
 
-    trips += CheckEdges(bl, tr, "ALL")
+    CheckEdges(bl, tr, type, t0, t1, withFare, results)
+
+    # We only want the number of unique trips.
+    trips = results['trips']
     trips = [int(x) for x in trips]
-    print "Trips: ", len(set(trips)), list(set(sorted(trips)))
-    return len(trips)
+    uniqueTrips = list(set(sorted(trips)))
+    results['trips'] = uniqueTrips
 
-def TripsStartedInRect(bl, tr):
+    if withFare:
+        print "Trips: ", len(uniqueTrips), "$%d" % int(results['fare']), uniqueTrips
+    else:
+        print "Trips: ", len(uniqueTrips), uniqueTrips
+
+def TripsGoingThroughRect(bl, tr, results):
+    '''
+    Query how many trips passed through a given geo-rect, as defined by the
+    (bottom,left) and (top,right) points and returns it in the passed in
+    dictionary.
+    '''
+    return QueryRect(bl, tr, "ALL", "-inf", "inf", False, results)
+
+def TripsStartedInRect(bl, tr, results):
     '''
     Query how many trips started in a given geo-rect, as defined by the
     (bottom,right) and (top,left) points.
     '''
+    return QueryRect(bl, tr, "BEGIN", "-inf", "inf", False, results)
 
-def TripsEndedInRect(bl, tr):
+def TripsEndedInRect(bl, tr, results):
     '''
     Query how many trips ended in a given geo-rect, as defined by the
     (bottom,right) and (top,left) points.
     '''
+    return QueryRect(bl, tr, "END", "-inf", "inf", True, results)
 
-def TripsAtTime(t):
+def TripsAtTime(t0, t1, results):
     '''
     Query how many trips were occurring at a given point in time.
     '''
-    # numTrips += r.zcount(key, "-inf", "inf")
+    return QueryRect((-90, -180), (79, 179), "ALL", t0, t1, True, results)
 
 def usage():
     print '  usage: TripQuery.py opts'
@@ -214,9 +237,10 @@ def main(argv):
     queryTime = False
     bl = None
     tr = None
-    t = None
+    t0 = None
+    t1 = None
     try:
-        opts, args = getopt.getopt(argv,"haset",["help","bl=","tr=","time="])
+        opts, args = getopt.getopt(argv,"haset",["help","bl=","tr=","t0=", "t1="])
     except getopt.GetoptError:
         usage()
         sys.exit(2)
@@ -236,8 +260,10 @@ def main(argv):
             bl = [int(n) for n in arg.split(',')]
         elif opt in ("--tr"):
             tr = [int(n) for n in arg.split(',')]
-        elif opt in ("--time"):
-            t = float(arg)
+        elif opt in ("--t0"):
+            t0 = float(arg)
+        elif opt in ("--t1"):
+            t1 = float(arg)
 
     # Sanity check some combinations
     if queryRectAll or queryRectStart or queryRectEnd:
@@ -257,18 +283,30 @@ def main(argv):
         print "Warning: rects are specified without a relevant query."
 
     if queryTime:
-        if t is None:
-            print "Error: option t requires --time to be specified."
+        if t0 is None or t1 is None:
+            print "Error: option t requires t0 and t1 to be specified."
             sys.exit(2)
-    elif t is not None:
-        print "Warning: time is specified without 't' being specified."
+    elif t0 is not None or t1 is not None:
+        print "Warning: t0 or t1 is specified without 't' being specified."
 
     # XXX: currently I'm leaving this open to compound queries, like
     # if they specify a time and a rect, we should find how many trips
     # went through that rect at that time.
+
+    resultsDict = {}
+    resultsDict['trips'] = []
+    resultsDict['fare'] = 0
     if queryRectAll:
-        TripsInRect(bl, tr)
+        TripsGoingThroughRect(bl, tr, resultsDict)
+
+    if queryRectStart:
+        TripsStartedInRect(bl, tr, resultsDict)
+
+    if queryRectEnd:
+        TripsEndedInRect(bl, tr, resultsDict)
+
+    if queryTime:
+        TripsAtTime(t0, t1, resultsDict)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
-
